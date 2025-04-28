e8be8086cf4a691b4327dc2d10e628ccf9ab563b
Julio Ruiz Villavicencio <julio.ruiz@utec.edu.pe>
2025-04-28 17:37:45 -0500

    Subiendo archivo c++

---
+++ practica1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Persona{
+public:
+    string nombre;
+
+    explicit Persona(const string &nombre) : nombre(nombre) {
+
+    }
+};
+
+
+
+int main(){
+   cout<<"hello world"<<endl;
+   return 0;
+}
